7715e484da5dd4cb05fa5203813fb56b274069c9
Jibin <jibinbiju2125@gmail.com>
2026-07-24 19:29:21 +0530

    Add microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -108,6 +108,22 @@ fbq('track','PageView');
           />
         </noscript>
         {/* End Meta Pixel */}
+
+        {/* Microsoft Clarity */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xrhc6no6z7");
+            `,
+          }}
+        />
+        {/* End Microsoft Clarity */}
       </head>
       <body className={`${dmSans.variable} ${inter.variable} ${nunito.variable} font-sans antialiased`}>
         {/* Google Tag Manager (noscript) */}
